5f79296b24eb552fde081f3e1d123ce86be09140
Bary-G <b9059289@gmail.com>
2025-07-28 11:50:31 +0200

    0

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -5,7 +5,7 @@ A file that runs functions under Python3.
 """
 
 
-def to_str(n: float) -> int:
+def to_str(n: float) -> str:
     """A  type-annotated function to_str that takes a float n as argument and
     returns the string representation of the float."""
     if n >= 0:
