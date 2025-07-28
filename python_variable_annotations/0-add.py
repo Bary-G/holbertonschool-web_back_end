d6738e1b65c5c4d593e8afd304c3a8b163693c7b
Bary-G <b9059289@gmail.com>
2025-07-28 10:33:35 +0200

    0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module:
+A file that runs functions under Python3.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """A type-annotated function add that takes a float a and a float b as
+    arguments and returns their sum as a float."""
+    sumResult = a + b
+    return sumResult
