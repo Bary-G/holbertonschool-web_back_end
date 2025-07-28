e0508567ea89a6ad7a9a149c6df7b743e9d8d44f
Bary-G <b9059289@gmail.com>
2025-07-28 11:49:14 +0200

    0

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module:
+A file that runs functions under Python3.
+"""
+
+
+def to_str(n: float) -> int:
+    """A  type-annotated function to_str that takes a float n as argument and
+    returns the string representation of the float."""
+    if n >= 0:
+        return int(n)
+    elif n == int(n):
+        return int(n) - 1
+    else:
+        return int(n) - 0
