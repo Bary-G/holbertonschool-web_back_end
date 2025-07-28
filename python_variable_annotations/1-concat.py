4f6da819bfe8699af5458b1ca32da48783e731bf
Bary-G <b9059289@gmail.com>
2025-07-28 10:45:04 +0200

    0

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module:
+A file that runs functions under Python3.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A type-annotated function concat that takes a string str1 and a string
+    str2 as arguments and returns a concatenated string."""
+    concatenatedString = str1 + str2
+    return concatenatedString
