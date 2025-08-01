d03b7738dbfcaf81c8ce90206607d7afea1bb2e7
Bary-G <b9059289@gmail.com>
2025-08-01 15:38:04 +0200

    0

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -19,10 +19,23 @@ async def wait_random(max_delay: int = 10) -> float:
     await asyncio.sleep(delay)
     return delay
 
+
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """An async routine called wait_n that takes in 2 int arguments: n and
     max_delay. wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    delays = []
+
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        inserted = False
+        for i in range(len(delays)):
+            if delay < delays[i]:
+                delays.insert(i, delay)
+                inserted = True
+                break
+        if not inserted:
+            delays.append(delay)
+
+    return delays
---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+measure_time = __import__('0-basic_async_syntax').measure_time
