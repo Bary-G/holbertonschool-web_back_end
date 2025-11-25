d7f7fa469827678916da0f9dbc476def89a520df
Bary-G <b9059289@gmail.com>
2025-11-25 13:52:48 +0100

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module: A type-annotated function sum_mixed_list which takes a list
+mxd_lst of floats as argument and returns their sum as a float
+"""
+import typing
+
+def sum_mixed_list(mxd_lst: typing.List[float]) -> float:
+    """
+    A type-annotated function sum_mixed_list which takes a list
+    mxd_lst of floats as argument and returns their sum as a float
+    """
+    return sum(mxd_lst)
