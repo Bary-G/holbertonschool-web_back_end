13f6a2f02ff0090dd4b340176dd4f31170e9bf09
Bary-G <b9059289@gmail.com>
2025-11-25 09:32:52 +0100

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module: A type-annotated function concat that takes
+a string str1 and a string str2 as arguments and returns
+a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A type-annotated function concat that takes
+    a string str1 and a string str2 as arguments and returns
+    a concatenated string
+    """
+    return f"{str1}{str2}"
