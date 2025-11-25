b54d16244fe5451a846951fc2adf8d1066f23f2c
Bary-G <b9059289@gmail.com>
2025-11-25 15:06:36 +0100

    task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -7,7 +7,7 @@ typing: This module is used to annotate in a list
 import typing
 
 
-def sum_mixed_list(mxd_lst: typing.List[float]) -> float:
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[float]]) -> float:
     """
     A type-annotated function sum_mixed_list which takes a list
     mxd_lst of floats as argument and returns their sum as a float
