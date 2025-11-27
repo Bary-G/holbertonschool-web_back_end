a375d4a68979e91d8891d8f52f918a0e6d5e8dfb
Bary-G <b9059289@gmail.com>
2025-11-27 08:55:55 +0100

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module: Returns the list of all the delays (float values)
+"""
+import random
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int):
+    """
+    Returns the list of all the delays (float values)
+    """
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*coroutines)
+    return sorted(results)
