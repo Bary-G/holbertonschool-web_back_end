ba3dc7ea4fc86f0fb6fd68835dab58c8d4a38545
Bary-G <b9059289@gmail.com>
2025-11-27 10:05:29 +0100

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,11 @@
 Module: Returns the list of all the delays (float values)
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns the list of all the delays (float values)
     """
