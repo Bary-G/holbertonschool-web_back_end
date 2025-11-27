69fb9e620f75e0c7abbb61abbba734bd47ce3c07
Bary-G <b9059289@gmail.com>
2025-11-27 09:36:46 +0100

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,10 +6,21 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int):
     """
     Returns the list of all the delays (float values)
     """
     coroutines = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*coroutines)
-    return sorted(results)
+    return selection_sort(results)
+
+def selection_sort(lst):
+    """Sorts a list by ascendant order using selection sort"""
+    n = len(lst)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if lst[j] < lst[min_index]:
+                min_index = j
+        lst[i], lst[min_index] = lst[min_index], lst[i]
+    return lst
