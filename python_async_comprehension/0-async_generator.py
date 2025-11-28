6b1b5261a614f8e97f9c25f7020aa9e54ac17564
Bary-G <b9059289@gmail.com>
2025-11-28 10:03:00 +0100

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module: An asynchronous coroutine that loop 10 times,
+each time asynchronously wait 1 second, then yield a random number
+between 0 and 10
+"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    An asynchronous coroutine that loop 10 times,
+    each time asynchronously wait 1 second, then yield a random number
+    between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
