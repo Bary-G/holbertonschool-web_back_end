603f7b70b7bf3a07d82874eddd354f7a803fc59d
Bary-G <b9059289@gmail.com>
2025-12-18 10:24:15 +0100

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-        if (promise) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            });
-        } else {
-            reject(new Error());
-        }
-        console.log("Got a response from the API")
-    });
+    return promise
+        .then(result => ({ status: 200, body: "success" }))
+        .catch(error => new Error())
+        .finally(() => { console.log("Got a response from the API") });
 }
